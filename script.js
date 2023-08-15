e2dac0967f72a2fcc4c89bb8104634582b93552f
Clean8876 <120701325+Clean8876@users.noreply.github.com>
2023-08-15 15:49:02 +0530

    update 1.2

--- script.js
+++ script.js
@@ -5,24 +5,63 @@ const transactionNet = document.getElementById('total-net')
 
 let transactions =[];
 
-document.getElementById("addtrans").addEventListener("click",addtransaction);
+document.getElementById("addtrans").addEventListener("click",addTransaction);
 
-function addtransaction() {
+function addTransaction() {
     const desc = document.getElementById('description').value;
-    const amount = document.getElementById('amount').value;
+    const amount = parseFloat(document.getElementById('amount').value);
     const transtype = document.getElementById('trans-type').value;
 
     if(desc && isNaN(amount)){
         const  transaction = {desc,amount,type:transtype};
         transactions.push(transaction);
-        updatetrans();
-        updateinsight();
-        clearform();
+        updateTrans();
+        updateInsight();
+        clearForm();
     }
     
 };
-const updatetrans()=>{
+const updateTrans=() =>{
     transactionList.innerHTML=""
-    
-}
+    const transItem = transactions.map((transaction,index)=>{
+        return `
+      <li>
+        <span>${transaction.desc}</span>
+        <span class="${transaction.type}">${transaction.amount.toFixed(2)}</span>
+        <button onclick="removeTransaction(${index})">Remove</button>
+      </li>
+      `;
+    });
+    transactionList.innerHTML=transItem.join("");
+
+};
+const removeTransaction = (index)=>{
+    transactions.splice(index,1);
+    updateTrans();
+    updateInsight();
+
+};
+
+const updateInsight=()=>{
+    const totalIncome = transactions.reduce((sum,transaction)=>{
+        return transaction.type==='income'? sum + transaction.amount:sum;
+    },0);
+    const totalExpense = transactions.reduce((transaction,sum)=>{
+        return transaction.type==='expense'? sum - transaction.amount:sum;
+
+    },0);
+    const netIncome = totalIncome-totalExpense;
+    transactionIncome.textContent=`$${totalIncome.toFixed(2)}`;
+    transactionExpense.textContent=`$${totalExpense.toFixed(2)}`;
+    transactionNet.textContent=`$${netIncome.toFixed(2)}`;
+};
+
+const clearForm=()=>{
+    document.getElementById("description").value="";
+    document.getElementById("amount").value="";
+
+};
+updateTrans();
+updateInsight();
+
 
